b2a3d64ffad7b958b093d071edcdc3d37881ac6b
wmp-tosiki <milpo99@crewmate.co.kr>
2020-05-23 09:30:43 +0900

    Updates

---
+++ precache-manifest.1c8c525ff227828f63a7b4ba66e3fcb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df86dc59763114b87715657d1b787a1",
+    "url": "/react_pc/index.html"
+  },
+  {
+    "revision": "8e15fa39916e8f1babbd",
+    "url": "/react_pc/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1abad1898d79271148f8",
+    "url": "/react_pc/static/js/2.21f4cdbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_pc/static/js/2.21f4cdbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e15fa39916e8f1babbd",
+    "url": "/react_pc/static/js/main.41e4c648.chunk.js"
+  },
+  {
+    "revision": "2b31221bf1b49b3bf8e2",
+    "url": "/react_pc/static/js/runtime-main.570c6ab4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_pc/static/media/logo.5d5d9eef.svg"
+  }
+]);
